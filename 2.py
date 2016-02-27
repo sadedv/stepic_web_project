1cba95afac85c327c6c7aef1cc1a76d7b1570a11
sadedv <sadedv@gmail.com>
2016-02-27 22:08:12 +0300

    sdfsdf

--- 2.py
+++ 2.py
@@ -1,30 +1,32 @@
-import asyncore                                                                 
-import socket                                                                   
-                                                                                
-class EchoHandler(asyncore.dispatcher_with_send):                               
-        def handle_read (self):                                                 
-                data = self.recv(1024)                                               
-                if data:
-                if data == "close" or "Close": conn.close()
-                print(data)
-                conn.send(data)
-                conn.close() 
-                                                     
-                                                                                
-class EchoServer(asyncore.dispatcher):                                          
-        def __init__(self, host, port):                                         
-                asyncore.dispatcher.__init__(self)  
-                self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.set_reuse_addr()
-                self.bind((host, port))
-                self.listen(10)
-
-        def handle_accept(self):
-        	pair = self.accept()
-        	if pair is not None:
-        		sock, addr = pair
-        		print "Incoming connection from %s" % repr(addr)
-        		handler = EchoHandler(sock)
-
-server = EchoHandler('0.0.0.0', 2222)
-asyncore.loop()
+import socket                                                                                                
+import asyncore                                                                                              
+                                                                                                             
+class EchoHandler(asyncore.dispatcher_with_send):                                                            
+                                                                                                             
+        def handle_read(self):                                                                              
+                data = self.recv(1024)                                                                      
+                if data:                                                                                    
+                        if data == 'close':                                                                  
+                                self.close()                                                                
+                        self.send(data)                                                                      
+                                                                                                             
+class EchoServer(asyncore.dispatcher):                                                                      
+                                                                                                             
+        def __init__(self, host, port):                                                                      
+                asyncore.dispatcher.__init__(self)                                                          
+                self.create_socket(socket.AF_INET, socket.SOCK_STREAM)                                      
+                self.set_reuse_addr()                                                                        
+                self.bind((host, port))                                                                      
+                self.listen(10)                                                                              
+                                                                                                             
+        def handle_accept(self):                                                                            
+                pair = self.accept()                                                                        
+                if pair is not None:                                                                        
+                        sock, addr = pair                                                                    
+                        print('Incoming connection from %s' % repr(addr))                                    
+                        handler = EchoHandler(sock)                                                          
+                else:                                                                                        
+                        return                                                                              
+                                                                                                             
+server = EchoServer('0.0.0.0', 2222)                                                                        
+asyncore.loop()    

f9f58cc07fd601e7c2b9581080c86745deaf3d95
sadedv <sadedv@gmail.com>
2016-03-02 17:04:21 +0300

    f-b 1

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm /etc/nginx/sites-enabled/default
-/etc/init.d/nginx restart
-#ln -s /home/box/web/etc/guniconr.conf /etc/gunicorn/test.conf
-#/etc/init.d/gunicorn restart
+sudo rm -r /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
